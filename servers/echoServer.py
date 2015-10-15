9605cb817bb95bb03c66a70173b043fd04c6af52
Daniel Castro <DanCasSig@gmail.com>
2015-10-14 19:50:20 -0500

    Adds basic echo server on port 8000

---
+++ servers/echoServer.py
@@ -0,0 +1,28 @@
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor
+
+
+class Echo(Protocol):
+    def dataReceived(self, data):
+        data2 = data.rstrip()
+
+        if data2 == 'a':
+            self.transport.write('Command a selected\n')
+        elif data2 == 'b':
+            dad2 = self.transport
+        elif data2 == 'exit':
+            print 'Connection terminated'
+            self.transport.loseConnection()
+        else:
+            self.transport.write('server: ' + data)
+
+
+def main():
+    f = Factory()
+    f.protocol = Echo
+    reactor.listenTCP(8000, f)
+    reactor.run()
+
+
+if __name__ == '__main__':
+    main()
